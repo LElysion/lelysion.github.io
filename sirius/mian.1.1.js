fc76205eeccd8f40e21e2357ad46169bda0d42dc
LElysion <imoe2010@163.com>
2020-04-14 17:31:32 +0800

    新增参数控制渲染数量-num

--- sirius/mian.1.1.js
+++ sirius/mian.1.1.js
@@ -1,14 +1,26 @@
-(function() {
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) { return pair[1]; }
+  }
+  return (false);
+}
+(function () {
   let mode = 1;
+
+  let NUMBERS = getQueryVariable('num');
 
   let canvas = document.getElementById('canvas');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  window.onresize = function() {
+  window.onresize = function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
   }
-  canvas.addEventListener('click', function() {
+  canvas.addEventListener('click', function () {
     mode = mode == 1 ? 2 : 1;
   })
   let context = canvas.getContext('2d');
@@ -41,19 +53,19 @@
       return canvas.height;
     },
     random(min, max, type = 'INT', plus = 1) {
-      if(type === 'INT') 
-      return parseInt(Math.random() * (max - min) + min);
-      if(type === 'FIXED')
-      return (Math.random() * (max - min) + min).toFixed(2) * plus;
+      if (type === 'INT')
+        return parseInt(Math.random() * (max - min) + min);
+      if (type === 'FIXED')
+        return (Math.random() * (max - min) + min).toFixed(2) * plus;
     },
     clearCanvas() {
       context.clearRect(0, 0, this.getWidth(), this.getHeight());
     },
     createCircles() {
       let _this = this;
-      let numbers = 50;
+      let numbers = NUMBERS || 50;
       let res = [];
-      for(let i = 0; i< numbers; i++) {
+      for (let i = 0; i < numbers; i++) {
         let R = Math.floor(Math.random() * 255);
         let G = Math.floor(Math.random() * 255);
         let B = Math.floor(Math.random() * 255);
@@ -66,7 +78,7 @@
           radius: radius,
           color: `rgb(${R}, ${G}, ${B})`,
           startAngle: 0,//开始弧度
-          endAngel: Math.PI*2,//结束弧度
+          endAngel: Math.PI * 2,//结束弧度
           anticlockwise: true,//逆时针， false:顺时针
           animateTimes: 0
         })
@@ -91,21 +103,21 @@
     animate(circles) {
       let _this = this;
       window.cancelAnimationFrame(timer);
-      timer = window.requestAnimationFrame(function() {
+      timer = window.requestAnimationFrame(function () {
         _this.clearCanvas();
         circles.forEach(circle => {
           circle.animateTimes += 1;
           circle.x += circle.vx;//_this.random(0.05, 0.15, 'FIXED');
           circle.y += circle.vy;//_this.random(0.05, 0.15, 'FIXED');
           circle.globalAlpha -= _this.vglobalAlpha;
-          if(circle.animateTimes%360 === 0) {
+          if (circle.animateTimes % 360 === 0) {
             console.log('触发')
             circle.vx = circle.vx + _this.random(0.01, 1, 'FIXED') + (_this.random(0, 100) > 50 ? -1 : 1)
             circle.vy = circle.vy + _this.random(0.01, 1, 'FIXED') + (_this.random(0, 100) > 50 ? -1 : 1)
           }
 
-          if(mode === 1) {
-            if(circle.x >= _this.getWidth() + circle.radius || circle.y < -circle.radius) {
+          if (mode === 1) {
+            if (circle.x >= _this.getWidth() + circle.radius || circle.y < -circle.radius) {
               circle.animateTimes = 0;
               let R = Math.floor(Math.random() * 255);
               let G = Math.floor(Math.random() * 255);
@@ -116,30 +128,30 @@
               let s2 = _this.random(0.01, 2, 'FIXED');
               circle.vx = s1 //* (_this.random(0, 100) > 50 ? -1 : 1);
               circle.vy = s2 //* (_this.random(0, 100) > 50 ? -1 : 1);
-              if(_this.random(0, 10) > 5) {
-                circle.x = _this.random(0, 0, 'FIXED') - circle.radius; 
+              if (_this.random(0, 10) > 5) {
+                circle.x = _this.random(0, 0, 'FIXED') - circle.radius;
                 circle.y = _this.random(0, _this.getHeight(), 'FIXED');
               } else {
-                circle.y = _this.random(0, 0, 'FIXED') - circle.radius; 
+                circle.y = _this.random(0, 0, 'FIXED') - circle.radius;
                 circle.x = _this.random(0, _this.getWidth(), 'FIXED')
               }
             }
-          } else if(mode === 2){
-            if(
+          } else if (mode === 2) {
+            if (
               (circle.x <= circle.radius || circle.x > _this.getWidth() - circle.radius)
-              || (circle.y <= circle.radius || circle.y > _this.getHeight() - circle.radius )
+              || (circle.y <= circle.radius || circle.y > _this.getHeight() - circle.radius)
             ) {
               let R = Math.floor(Math.random() * 255);
               let G = Math.floor(Math.random() * 255);
               let B = Math.floor(Math.random() * 255);
-              if(circle.animateTimes > 30) {
+              if (circle.animateTimes > 30) {
                 circle.animateTimes = 0;
                 circle.color = `rgb(${R}, ${G}, ${B})`;
               }
-              if(circle.x <= circle.radius) circle.x = circle.radius;
-              if(circle.x >= _this.getWidth() - circle.radius) circle.x = _this.getWidth() - circle.radius;
-              if(circle.y <= circle.radius) circle.y = circle.radius;
-              if(circle.y > _this.getHeight() - circle.radius) circle.y = _this.getHeight() - circle.radius;
+              if (circle.x <= circle.radius) circle.x = circle.radius;
+              if (circle.x >= _this.getWidth() - circle.radius) circle.x = _this.getWidth() - circle.radius;
+              if (circle.y <= circle.radius) circle.y = circle.radius;
+              if (circle.y > _this.getHeight() - circle.radius) circle.y = _this.getHeight() - circle.radius;
               let s1 = _this.random(0.01, 2, 'FIXED');
               let s2 = _this.random(0.01, 2, 'FIXED');
               circle.vx = s1 * (_this.random(0, 100) > 50 ? -1 : 1);
